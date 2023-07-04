7856a2cacf8cc68df37d175addf135c73b29737f
elsie <elsie.hsu@onelab.tw>
2023-07-04 20:51:54 +0800

    not init commit haha

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,68 @@
+<script>
+</script>
+
+<footer class="footer">
+	<div class="wrapper">
+		<div class="footer-top">
+			<div class="logo">LOGO</div>
+			<div class="icons">
+				<div>123</div>
+				<div>123</div>
+				<div>123</div>
+				<div>123</div>
+			</div>
+			<div class="cta">
+				<div class="cta-title">Stay up to date</div>
+				<div class="cta-content">
+					with new challenges, featured solutions, selected articles and our
+					latest news
+				</div>
+				<input type="text" placeholder="email@example.com" />
+				<button>Subscribe</button>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<style>
+	.footer {
+		display: flex;
+		flex-direction: column;
+		background-color: rgb(255, 255, 255);
+	}
+
+	.wrapper {
+		margin: 30px 15px;
+	}
+
+	.footer-top {
+		display: flex;
+		flex-direction: column;
+		gap: 30px;
+	}
+
+	.logo {
+		display: flex;
+		justify-content: center;
+	}
+
+	.icons {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 10px;
+	}
+
+	.cta {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.cta-title {
+	}
+	.cta-content {
+		text-align: center;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,60 @@
+<script>
+	import IconLogo from '../components/icons/logo/IconLogo.svelte';
+</script>
+
+<header>
+	<div class="logo">
+		<IconLogo />
+	</div>
+	<div class="menu">
+		<div class="menu-icon">
+			<div />
+			<div />
+			<div />
+		</div>
+	</div>
+</header>
+
+<style>
+	header {
+		height: 55px;
+		border-bottom: 1px solid gray;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.logo {
+		border-right: 1px solid gray;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		padding: 0 14px;
+		/* box-sizing: border-box; */
+	}
+
+	.menu {
+		height: 100%;
+		width: 55px;
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.menu-icon {
+		height: 24px;
+		width: 33px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.menu-icon div {
+		height: 4px;
+		border-radius: 2px;
+		width: 100%;
+		background-color: black;
+	}
+</style>
